960e650427480c78200556c36398a58720170e6b
GeMy119 <teba7033@gmail.com>
2024-04-26 04:30:53 +0300

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  // production: true,
   backEndUrl: "https://doc-learn-back-end.vercel.app"
 };
 
